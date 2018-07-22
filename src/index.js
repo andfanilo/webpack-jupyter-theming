149deaa3f16458b39127c57086ffb276b31b6fe7
Fanilo ANDRIANASOLO <andfanilo@gmail.com>
2018-07-22 15:57:36 +0200

    :sparkles: Retrieve Jupyter global variable and catch cell creation deletion events

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import "./style.css";
 
-console.log("Hello Jupyter");
+console.log("Hello custom.js file");
+
+define(["jquery", "base/js/namespace", "base/js/events"], function(
+  $,
+  Jupyter,
+  events
+) {
+  ("use strict");
+  console.log(Jupyter.notebook);
+  console.log(Jupyter.notebook.get_cells());
+
+  events.on("create.Cell", function() {
+    console.log("Created a cell");
+  });
+
+  events.on("delete.Cell", function() {
+    console.log("Deleted a cell");
+  });
+});
